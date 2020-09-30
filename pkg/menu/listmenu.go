8fcaef40d89ff5772d3892a04f2f2f14aecdf951
Akatsuki-py <bluejapan73+dev@gmail.com>
2020-09-30 09:26:29 +0900

    wip

--- pkg/menu/listmenu.go
+++ pkg/menu/listmenu.go
@@ -9,3 +9,9 @@ type ListMenu struct {
 	X, Y, Z, W, H int
 	Cache         *ebiten.Image
 }
+
+// DisplayListMenuIDLoop list menu でユーザーの入力を待って対応する処理
+func DisplayListMenuIDLoop() bool {
+	selected := true
+	return selected
+}
