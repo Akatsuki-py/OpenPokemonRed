b82dc6e10f56859cca13c0e5f88bffdced328720
Akatsuki-py <bluejapan73+dev@gmail.com>
2020-10-03 05:29:14 +0900

    wip

--- pkg/audio/music.go
+++ pkg/audio/music.go
@@ -1,1 +1,34 @@
 package audio
+
+import (
+	"net/http"
+
+	"github.com/hajimehoshi/ebiten/audio/mp3"
+	"github.com/rakyll/statik/fs"
+)
+
+const (
+	MUSIC_PALLET_TOWN uint = iota
+)
+
+var MusicMap = newMusicMap()
+
+func newMusicMap() map[uint]*mp3.Stream {
+	musicMap := map[uint]*mp3.Stream{}
+	FS, _ := fs.New()
+	musicMap[MUSIC_PALLET_TOWN] = newMP3(FS, "/PalletTown.mp3")
+	return musicMap
+}
+
+func newMP3(fs http.FileSystem, path string) *mp3.Stream {
+	f, err := fs.Open(path)
+	if err != nil {
+		return nil
+	}
+	defer f.Close()
+	stream, err := mp3.Decode(audioContext, f)
+	if err != nil {
+		return nil
+	}
+	return stream
+}
