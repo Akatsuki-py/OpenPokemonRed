77f4d4f686c35d7213d3717b44ae2ae85fbd7ba4
pokemium <bluejapan73+dev@gmail.com>
2020-10-27 03:38:05 +0900

    wip

--- pkg/script/title.go
+++ pkg/script/title.go
@@ -1,7 +1,9 @@
 package script
 
 import (
+	"fmt"
 	"pokered/pkg/audio"
+	"pokered/pkg/data/pokemon"
 	"pokered/pkg/store"
 	"pokered/pkg/util"
 
@@ -22,6 +24,24 @@ var (
 	title Title
 )
 
+var titleMons = []uint{
+	pokemon.CHARMANDER,
+	pokemon.BULBASAUR,
+	pokemon.WEEDLE,
+	pokemon.NIDORAN_M,
+	pokemon.SCYTHER,
+	pokemon.PIKACHU,
+	pokemon.CLEFAIRY,
+	pokemon.RHYDON,
+	pokemon.ABRA,
+	pokemon.GASTLY,
+	pokemon.DITTO,
+	pokemon.PIDGEOTTO,
+	pokemon.ONIX,
+	pokemon.PONYTA,
+	pokemon.MAGIKARP,
+}
+
 type Title struct {
 	counter     int
 	img         *ebiten.Image
@@ -30,6 +50,8 @@ type Title struct {
 	red         *ebiten.Image
 	redWithBall *ebiten.Image
 	redBall     *ebiten.Image
+	monID       uint
+	mon         *ebiten.Image
 }
 
 func titleCopyright() {
@@ -76,11 +98,20 @@ func titlePokemonRed() {
 	}
 	util.DrawImage(store.TileMap, title.img, 0, 0)
 
+	if title.mon == nil {
+		title.monID = pokemon.CHARMANDER
+		name := pokemon.Name(title.monID)
+		path := fmt.Sprintf("/%s_1.png", name)
+		title.mon = util.OpenImage(store.FS, path)
+	}
+	util.DrawImage(store.TileMap, title.mon, 5, 10)
+
 	if title.red == nil {
 		title.red = util.OpenImage(store.FS, "/title_red_1.png")
 		title.redWithBall = util.OpenImage(store.FS, "/title_red_0.png")
 		title.redBall = util.OpenImage(store.FS, "/title_red_ball.png")
 	}
 	util.DrawImagePixel(store.TileMap, title.redWithBall, 82, 80)
+
 	title.counter++
 }
